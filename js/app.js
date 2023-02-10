7572e23408dd0d79fd85e77bde22025547dd54a0
longo2002 <lombardo.fra2002@gmail.com>
2023-02-10 15:47:15 +0100

    grid push

---
+++ js/app.js
@@ -0,0 +1,45 @@
+console.log('griglia')
+let btnEl = document.getElementById("bottone")
+
+bottone.addEventListener("click",
+    function myFunction() {
+        btnEl.classList.add("nodisplay")
+
+
+        let gridSide = 10
+        let playArea = gridSide * gridSide
+        const gridEl = document.querySelector('.griglia')
+
+        for (let i = 0; i < playArea; i++) {
+            let num = i + 1
+            console.log(num)
+            let divString = `<div class="cella" style="width: calc(100% / ${gridSide});" >${num}</div>`
+            gridEl.innerHTML += divString
+
+        }
+
+        const cellEl = document.querySelectorAll('.cella')
+
+        for (let i = 0; i < cellEl.length; i++) {
+            const cell = cellEl[i]
+
+            cell.addEventListener('click', function () {
+                cell.classList.toggle("indicatore")
+                console.log(i + 1)
+            })
+        }
+    }
+)
+
+let refresh = document.getElementById("refresh-btn")
+refresh.addEventListener("click",
+    function refreshFunction() {
+        window.location.reload(true)
+    }
+)
+
+
+
+
+
+
